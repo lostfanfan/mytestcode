52d39824ea27c52c48b5f3b2a482cc7ec17dd523
lostfanfan <pigflypig@gmail.com>
2011-01-29 01:20:43 +0800

    first modify

--- main.c
+++ main.c
@@ -5,8 +5,10 @@ int main()
 	int a;
 	printf("please input a number:");
 	scanf("%d", %a);
-	if(a!=0)
-		printf("a is %d\n", a);
+	if(a>0)
+		printf("a is negative\n", a);
+	else if(a<0)
+		printf("a is positive\n", a);
 	else
 		printf("a is zero!\n");
 	return 0;
