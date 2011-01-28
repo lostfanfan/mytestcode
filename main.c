fb25ee30a33d86a25f2251141000e6e3c37a9b8b
lostfanfan <pigflypig@gmail.com>
2011-01-28 23:51:10 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("please input a number:");
+	scanf("%d", %a);
+	if(a!=0)
+		printf("a is %d\n", a);
+	else
+		printf("a is zero!\n");
+	return 0;
+}
+
